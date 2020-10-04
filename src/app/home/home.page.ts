bed98fb1f15df13590a5e87dc5e2afca21435e06
kunj.patel <kunj.patel@streebo.com>
2020-10-04 08:48:20 +0530

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -36,15 +36,62 @@ export class HomePage {
   firstName=new FormControl('');
   surname=new FormControl('');
   fatherName=new FormControl('');
+  dotbirth=new FormControl('');
+  age=new FormControl('');
+  gender=new FormControl('');
+  identityCardType=new FormControl('');
+  cardNumber=new FormControl('');
+  eduLevel=new FormControl('');
+  incomeStatus=new FormControl('');
+  dailyIncome=new FormControl('');
+  maritalStatus=new FormControl('');
+  bloodGrop=new FormControl('');
+  mono=new FormControl('');
+  spouseName=new FormControl('');
+  email=new FormControl('');
+
+  siteId=new FormControl('');
+  siteName=new FormControl('');
+  jobType=new FormControl('');
+  jobCharacteristic=new FormControl('');
+  durationTo=new FormControl('');
+  durationFrom=new FormControl('');
+  nearByFaculty=new FormControl('');
+
+  bankName=new FormControl('');
+  bankaccount=new FormControl('');
+  isfccode=new FormControl('');
+  beneficiaryName=new FormControl('');
+  presentAddress1=new FormControl('');
+  presentAddress2=new FormControl('');
+  presentState1=new FormControl('');
+  presentDistrict1=new FormControl('');
+  PermanentState2=new FormControl('');
+  PermanentDistrict2=new FormControl('');
+  premanentAddress1=new FormControl('');
+  premanentAddress2=new FormControl('');
+
+  noOfMem=new FormControl('');
+  noOfOrg=new FormControl('');
+  noOfUnorg=new FormControl('');
+  incomeIndication=new FormControl('');
+  eduStatus=new FormControl('');
+  ageOfMarriedWomen=new FormControl('');
+  kwh=new FormControl('');
+  DrainageConnectivity=new FormControl('');
+  houseHold=new FormControl('');
+  problem=new FormControl('');
+  
+
+  height=new FormControl('');
+  weight=new FormControl('');
+  mass=new FormControl('');
 
   constructor() {
+    //this.DetailsFormgroup.migrantYes.chec
   }
 
   submitDetails() {
-  }
-
-  radioValue(value) {
-    console.log(value)
   }
 
   toggleGroup(index) {
